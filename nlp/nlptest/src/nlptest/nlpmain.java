1b7daa433d6ae3d211adb26daaa7daa3837f1a0b
nilesh.vaid <nilesh.vaid@gmail.com>
2016-12-15 20:34:30 -0800

    This is a test project.

---
+++ nlp/nlptest/src/nlptest/nlpmain.java
@@ -0,0 +1,8 @@
+package nlptest;
+
+public class nlpmain {
+	public static void main(String[] args) {
+	    // Prints "Hello, World" in the terminal window.
+	    System.out.println("Hello, World");
+	 }
+}
